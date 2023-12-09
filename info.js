2bdb9ece53e316795f799f2cf7cff3ef2e2368ac
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2023-12-09 17:42:46 +0330

    file created

---
+++ info.js
@@ -0,0 +1,54 @@
+const exp = useState({
+  skills: [
+    {
+      skill: "HTML",
+      skillLevel: "Expert",
+    },
+    {
+      skill: "CSS",
+      skillLevel: "Epert",
+    },
+    {
+      skill: "BOOTSTARP",
+      skillLevel: "Expert",
+    },
+    {
+      skill: "JAVASCRIPT",
+      skillLevel: "Intermediate",
+    },
+    {
+      skill: "REACT",
+      skillLevel: "Beginner",
+    },
+    {
+      skill: "GITHUB",
+      skillLevel: "Beginner",
+    },
+    {
+      skill: "NPM",
+      skillLevel: "Beginner",
+    },
+  ],
+  projectsDetails: [
+    {
+      title: "Personal Portilo",
+      imgSrc: "https://s6.uupload.ir/files/undraw_resume_re_hkth_fhrz.png",
+      gitLink: "github.com",
+      demoLink: "#",
+    },
+    {
+      title: "Project Management",
+      imgSrc: "https://s6.uupload.ir/files/undraw_team_up_re_84ok_eltr.png",
+      gitLink: "github.com",
+      demoLink: "#",
+    },
+    {
+      title: "Personal Blog",
+      imgSrc: "https://s6.uupload.ir/files/undraw_blogging_re_kl0d_47t.png",
+      gitLink: "github.com",
+      demoLink: "#",
+    },
+  ],
+});
+
+export default exp;

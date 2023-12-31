344c8df9429b67d7a30fd0d04463b4eb89ff2ac4
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2023-12-31 17:23:57 +0330

    links upadted

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.querySelector('.project-link').addEventListener('click', function(event) {
+document.querySelector('a').addEventListener('click', function(event) {
      event.preventDefault();
      window.open(this.href, '_blank');
  });
